305b0b9ff9d6195d144b11871c3b22de4ea9e99a
tomaspasto24 <tomaspasto24@gmail.com>
2021-09-01 10:45:41 -0300

    Actualización

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -4,24 +4,46 @@ namespace SRP
 {
     public class Libro
     {
+        public string Titulo { get ; private set; }
+        public string Autor { get ; private set; }
+        public string Codigo { get ; private set; }
+        public Ubicacion Ubicacion { get; private set; }
 
-        public string Titulo { get ; }
-        public string Autor { get ; }
-        public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
-        public string EstanteBiblioteca { get ; set; }
-
-        public Libro(String titulo, String autor, String codigo)
+        public Libro(String titulo, String autor, String codigo, String sector, String estante)
         {
             this.Titulo = titulo;
             this.Autor = autor;
             this.Codigo = codigo;
+            this.ubicacion = new Ubicacion(sector, estante);
+        }
+    }
+
+    public class Biblioteca
+    {
+        private string nombreBiblioteca { get; }
+        private List<Libro> listaLibros = new List<Libro>();
+
+        public Biblioteca(string nombreBiblioteca)
+        {
+            this.nombreBiblioteca = nombreBiblioteca;
         }
 
-        public void AlmacenarLibro(String sector, String estante)
+        public AlmacenarLibro(Libro libro)       
         {
-            this.SectorBiblioteca = sector;
-            this.EstanteBiblioteca = estante;
+            
+        }
+
+    }
+
+    public class Ubicacion
+    {
+        private string sector { get; set; }
+        private string estante { get; set; }
+
+        public Ubicacion(string sector, string estante)
+        {
+            this.sector = sector;
+            this.estante = estante;
         }
 
     }
